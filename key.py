3eb399f3f4025c1674f8ea0768a71dcb97550d3e
lujiaweigood <38624816+lujiaweigood@users.noreply.github.com>
2018-12-10 16:34:30 +0900

    Add files via upload

---
+++ key.py
@@ -0,0 +1,15 @@
+#project
+# This is a program to get the list of tweets with the given hashtag.
+
+import my_twitter_new
+
+CK = 'Enter the API key'
+
+CS = 'Enter the API secret key'
+
+AT = 'Enter the Access token'
+
+AS = 'Enter the Access token secret'
+
+
+
